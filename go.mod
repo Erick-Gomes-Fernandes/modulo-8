f547c6ca31db139685b03da02eab2529af992696
Erick Fernandes <erick.gomes.fernandes.egf@gmail.com>
2026-05-05 09:53:08 -0300

    Baixado echo

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module calculator
 
 go 1.25.6
+
+require github.com/labstack/echo/v5 v5.1.1
--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"calculator/math"
 	"fmt"
+
+	"github.com/labstack/echo/v5"
 )
 
 func main() {
+
+	// Echo instance
+	e := echo.New()
+
 	sum := math.Sum(5, 2)
 	fmt.Println(sum)
 
